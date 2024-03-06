f78630167d411259868f0ee7d4bbcc0d0b0098e1
Crampustallin <muha.br.ba@gmail.com>
2024-03-06 16:10:08 +0500

    fixed to make it more readable

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 const {crawlPage} = require('./scripts/crawl');
-const { Console } = require('node:console');
-const {argv, argv0} = require('node:process');
+const {argv} = require('node:process');
 
 let main = () => {
 	if(argv[2] && !argv[3]) {
 		const baseURL = argv[2];
 		console.log(`Strting crawling at ${baseURL}`);
 		let dots = '.';
+
 		const intrvalId = setInterval(() => {
 			if(dots === '...') {
 				dots = '.'
@@ -16,9 +16,15 @@ let main = () => {
 			process.stdout.write('\x1B[2J\x1B[0f');
 			console.log(`Strting crawling at ${baseURL}${dots}`);
 		}, 800);
-		crawlPage(baseURL).then(text => console.log(text))
-			.catch(err => console.log(err))
-			.finally(() => intrvalId.unref());
+
+		crawlPage(baseURL).then(text => {
+			intrvalId.unref();
+			console.log(text);
+		}).catch(err => { 
+			intrvalId.unref();
+			console.log(err);
+		});
+
 	} else {
 		console.error('ERROR: It has to be one baseURL argument');
 	}
